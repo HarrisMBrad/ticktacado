71e376ac40a0bc848baa621b661ced719548d9ab
HarrisMBrad <brforeal1@gmail.com>
2021-08-09 13:30:25 -0400

    edited App to myApp for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import myApp from './myApp.js';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<myApp />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+export default myApp;
