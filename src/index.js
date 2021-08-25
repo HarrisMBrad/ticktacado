a9e88c7936b49a17c2da05f499b14bfbead3b692
HarrisMBrad <bmichaelh13@gmail.com>
2021-08-25 16:29:07 -0400

    redux store set up, one minor error while compiling, commented in code for dev, hope to solve this in the future

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 // Import Board and Scoreboard views
